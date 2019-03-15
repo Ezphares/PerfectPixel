babdcc3074838d8e133e4adcd8fad91ad8ee4d8f
Jeppe Tarp <raider.j@gmail.com>
2019-03-16 00:39:11 +0100

    Added managerinterface for more clean access to managers and components

--- include/managerinterface/ManagerInterface.h
+++ include/managerinterface/ManagerInterface.h
@@ -1,12 +1,89 @@
 #pragma once
 
+#include <worldgraph/EntityManager.h>
+
+#include <memory>
+
 namespace perfectpixel {
 
-	class ManagerInterface
-	{
-	public:
-		ManagerInterface();
-		~ManagerInterface();
-	};
+	class ManagerInterface;
 
+	namespace physics
+	{
+		class PhysicsManager;
+
+		class TransformComponent;
+		class PhysicsComponent;
+		class ColliderComponent;
+	}
+
+	namespace behaviour
+	{
+		class BehaviourManager;
+	}
+
+	namespace graphics
+	{
+		class GraphicsManager;
+
+		class SpriteComponent;
+	}
+
+	namespace input
+	{
+		class InputManager;
+	}
+
+	namespace api
+	{
+		class ManagerInterface
+		{
+		public:
+			ManagerInterface();
+			~ManagerInterface();
+
+			/*
+			Manager Block
+			*/
+			void setPhysicsManager(physics::PhysicsManager *manager);
+			physics::PhysicsManager *Physics() const;
+
+			void setBehaviourManager(behaviour::BehaviourManager *manager);
+			behaviour::BehaviourManager *Behaviour() const;
+
+			void setGraphicsManager(graphics::GraphicsManager *manager);
+			graphics::GraphicsManager *Graphics() const;
+
+			void setInputManager(input::InputManager *manager);
+			input::InputManager *Input() const;
+
+			/**
+			Component Block
+			*/
+			// Default to behaviours
+			template<typename T>
+			T *getComponent(world::Entity entity)
+			{
+				return m_behaviourManager->getBehaviour<T>(world::Entity entity);
+			}
+
+			physics::TransformComponent *getTransformComponent(world::Entity entity);
+			template<> physics::TransformComponent *getComponent<physics::TransformComponent>(world::Entity entity) { return getTransformComponent(entity); }
+
+			physics::PhysicsComponent *getPhysicsComponent(world::Entity entity);
+			template<> physics::PhysicsComponent *getComponent<physics::PhysicsComponent>(world::Entity entity) { return getPhysicsComponent(entity); }
+
+			physics::ColliderComponent *getColliderComponent(world::Entity entity);
+			template<> physics::ColliderComponent *getComponent<physics::ColliderComponent>(world::Entity entity) { return getColliderComponent(entity); }
+
+			graphics::SpriteComponent *getSpriteComponent(world::Entity entity);
+			template<> graphics::SpriteComponent *getComponent<graphics::SpriteComponent>(world::Entity entity) { return getSpriteComponent(entity); }
+
+		private:
+			physics::PhysicsManager *m_physicsManager;
+			behaviour::BehaviourManager *m_behaviourManager;
+			graphics::GraphicsManager *m_graphicsManager;
+			input::InputManager *m_inputManager;
+		};
+	}
 }
--- src/managerinterface/ManagerInterface.cpp
+++ src/managerinterface/ManagerInterface.cpp
@@ -1,16 +1,82 @@
 #include <managerinterface/ManagerInterface.h>
 
+#include <physics/PhysicsManager.h>
+#include <behaviour/BehaviourManager.h>
+#include <graphics/GraphicsManager.h>
+#include <input/InputManager.h>
 
 namespace perfectpixel {
+	namespace api {
 
 
-	ManagerInterface::ManagerInterface()
-	{
+		ManagerInterface::ManagerInterface()
+		{
+		}
+
+
+		ManagerInterface::~ManagerInterface()
+		{
+		}
+
+		void ManagerInterface::setPhysicsManager(physics::PhysicsManager *manager)
+		{
+			m_physicsManager = manager;
+		}
+
+		perfectpixel::physics::PhysicsManager * ManagerInterface::Physics() const
+		{
+			return m_physicsManager;
+		}
+
+		void ManagerInterface::setBehaviourManager(behaviour::BehaviourManager *manager)
+		{
+			m_behaviourManager = manager;
+		}
+
+		perfectpixel::behaviour::BehaviourManager * ManagerInterface::Behaviour() const
+		{
+			return m_behaviourManager;
+		}
+
+		void ManagerInterface::setGraphicsManager(graphics::GraphicsManager *manager)
+		{
+			m_graphicsManager = manager;
+		}
+
+		perfectpixel::graphics::GraphicsManager * ManagerInterface::Graphics() const
+		{
+			return m_graphicsManager;
+		}
+
+		void ManagerInterface::setInputManager(input::InputManager *manager)
+		{
+			m_inputManager = manager;
+		}
+
+		perfectpixel::input::InputManager * ManagerInterface::Input() const
+		{
+			return m_inputManager;
+		}
+
+		perfectpixel::physics::TransformComponent * ManagerInterface::getTransformComponent(world::Entity entity)
+		{
+			return &m_physicsManager->getTransform(entity);
+		}
+
+		perfectpixel::physics::PhysicsComponent * ManagerInterface::getPhysicsComponent(world::Entity entity)
+		{
+			return m_physicsManager->hasPhysics(entity) ? &m_physicsManager->getPhysics(entity) : nullptr;
+		}
+
+		perfectpixel::physics::ColliderComponent * ManagerInterface::getColliderComponent(world::Entity entity)
+		{
+			return m_physicsManager->hasCollider(entity) ? &m_physicsManager->getCollider(entity) : nullptr;
+		}
+
+		perfectpixel::graphics::SpriteComponent * ManagerInterface::getSpriteComponent(world::Entity entity)
+		{
+			return m_graphicsManager->hasSprite(entity) ? &m_graphicsManager->getSprite(entity) : nullptr;
+		}
+
 	}
-
-
-	ManagerInterface::~ManagerInterface()
-	{
-	}
-
 }
