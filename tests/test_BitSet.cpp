cb46c707e0f546818aa409a441432b3fafd201d4
Jeppe Tarp <raider.j@gmail.com>
2019-08-05 23:24:45 +0200

    Add BitSet unit tests

---
+++ tests/test_BitSet.cpp
@@ -0,0 +1,93 @@
+#include <gtest/gtest.h>
+
+#include <Bedrock/BitSet.h>
+
+using namespace perfectpixel::bedrock;
+
+GTEST_TEST(test_BitSet, Create)
+{
+	BitSet allZero{ 4 };
+	for (int i = 0; i < 4; ++i)
+	{
+		ASSERT_FALSE(allZero[i]);
+	}
+
+	BitSet allOne{ 4 , true };
+	for (int i = 0; i < 4; ++i)
+	{
+		ASSERT_TRUE(allOne[i]);
+	}
+
+	BitSet alternate{ (uint8_t*)"\x0a", 4 };
+	ASSERT_EQ(false, alternate[0]);
+	ASSERT_EQ(true,  alternate[1]);
+	ASSERT_EQ(false, alternate[2]);
+	ASSERT_EQ(true,  alternate[3]);
+}
+
+GTEST_TEST(test_Bitset, Equality)
+{
+	BitSet a{ (uint8_t*)"\xaa", 8 };
+	BitSet b{ (uint8_t*)"\xaa", 8 };
+
+	ASSERT_TRUE(a == b);
+
+	BitSet c{ (uint8_t*)"\x55", 8 };
+
+	ASSERT_FALSE(a == c);
+	ASSERT_TRUE(a == ~c);
+
+	BitSet d{ (uint8_t*)"\xaa\xaa", 16 };
+
+	ASSERT_FALSE(a == d);
+}
+
+GTEST_TEST(test_BitSet, Negate)
+{
+	BitSet alternate{ (uint8_t*)"\x0a", 4 };
+	alternate.negate();
+	ASSERT_EQ(true, alternate[0]);
+	ASSERT_EQ(false, alternate[1]);
+	ASSERT_EQ(true, alternate[2]);
+	ASSERT_EQ(false, alternate[3]);
+
+	BitSet large{ 500 };
+	large.negate();
+	for (int i = 0; i < 500; ++i)
+	{
+		ASSERT_TRUE(large[i]);
+	}
+}
+
+GTEST_TEST(test_Bitset, And)
+{
+	BitSet a{ (uint8_t*)"\xbe\xff", 16 };
+	BitSet b{ (uint8_t*)"\xff\xaf", 16 };
+
+	ASSERT_TRUE((BitSet{ (uint8_t*)"\xbe\xaf", 16 }) == (a & b));
+}
+
+GTEST_TEST(test_Bitset, AndDifferentSizes)
+{
+	BitSet a{ (uint8_t*)"\xff\xff", 16 };
+	BitSet b{ (uint8_t*)"\xff", 8 };
+
+	ASSERT_TRUE((BitSet{ (uint8_t*)"\xff\x00", 16 }) == (a & b));
+}
+/* TODO: Or tests for when implemented
+GTEST_TEST(test_Bitset, Or)
+{
+	BitSet a{ (uint8_t*)"\xbe\x00", 16 };
+	BitSet b{ (uint8_t*)"\x00\xaf", 16 };
+
+	ASSERT_TRUE((BitSet{ (uint8_t*)"\xbe\xaf", 16 }) == (a | b));
+}
+
+GTEST_TEST(test_Bitset, OrDifferentSizes)
+{
+	BitSet a{ (uint8_t*)"\x00\xff", 16 };
+	BitSet b{ (uint8_t*)"\xff", 8 };
+
+	ASSERT_TRUE((BitSet{ (uint8_t*)"\xff\xff", 16 }) == (a | b));
+}
+*/
