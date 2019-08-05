fdb2e2a25499f793cf8261ff007986cdd014aa2d
Jeppe Tarp <raider.j@gmail.com>
2019-08-05 08:13:54 +0200

    Added vector swizzle function

--- tests/test_Vectors.cpp
+++ tests/test_Vectors.cpp
@@ -6,7 +6,7 @@
 
 using namespace perfectpixel::bedrock;
 
-GTEST_TEST(Vector, test_cross)
+GTEST_TEST(Vector, cross)
 {
 	Vector3 a = Vector3(3, 0, 0);
 	Vector3 b = Vector3(0, 5, 0);
@@ -16,3 +16,19 @@ GTEST_TEST(Vector, test_cross)
 	ExpectVectorEq(c_ab, Vector3(0, 0, 15));
 }
 
+GTEST_TEST(Vector, swizzle)
+{
+	Vector4 base{ 1, 2, 3, 4 };
+
+	Vector4 identity = Vector4::swizzle(base, { X, Y, Z, W });
+	ExpectVectorEq(Vector4{ 1, 2, 3, 4 }, identity);
+
+	Vector4 rotated = Vector4::swizzle(base, { W, X, Y, Z });
+	ExpectVectorEq(Vector4{ 4, 1, 2, 3 }, rotated);
+
+	Vector4 negated = Vector4::swizzle(base, { N_X, N_Y, N_Z, N_W });
+	ExpectVectorEq(Vector4{ -1, -2, -3, -4 }, negated);
+
+	Vector4 single = Vector4::swizzle(base, { Y, Y, Y, Y });
+	ExpectVectorEq(Vector4{ 2, 2, 2, 2 }, single);
+}
