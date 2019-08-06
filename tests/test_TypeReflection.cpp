3117ec7e310bceeebb4643a6c7e2dfa970be63eb
Jeppe Tarp <raider.j@gmail.com>
2019-08-06 21:47:51 +0200

    Added minimal type reflection test

---
+++ tests/test_TypeReflection.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+
+#include <Bedrock/TypeReflection.h>
+
+namespace some_namespace {
+	struct TestDummy {};
+
+	struct TestDummy2 {};
+}
+
+PP_TYPE_REFLECTION(some_namespace::TestDummy, TestDummy)
+PP_TYPE_REFLECTION(some_namespace::TestDummy2, TestDummy2)
+
+using namespace perfectpixel::bedrock;
+
+GTEST_TEST(test_TypeReflection, Null)
+{
+	ASSERT_EQ(0, typeID<nullptr_t>());
+#if PP_FULL_REFLECTION_ENABLED
+	ASSERT_EQ(std::string("nullptr_t"), typeName<nullptr_t>());
+#endif
+}
+
+GTEST_TEST(test_TypeReflection, Uniqueness)
+{
+	ASSERT_NE(0, typeID<some_namespace::TestDummy>());
+	ASSERT_NE(0, typeID<some_namespace::TestDummy2>());
+	ASSERT_NE(typeID<some_namespace::TestDummy2>(), typeID<some_namespace::TestDummy>());
+}
+
+#if PP_FULL_REFLECTION_ENABLED
+GTEST_TEST(test_TypeReflection, Names)
+{
+	ASSERT_EQ(std::string("TestDummy"), typeName<some_namespace::TestDummy>());
+	ASSERT_EQ(std::string("TestDummy2"), typeName<some_namespace::TestDummy2>());
+}
+#endif
