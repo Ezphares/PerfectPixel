17b74e6c9ed011831c253e015c4dc09629be1226
Jeppe Tarp <raider.j@gmail.com>
2020-11-22 13:38:49 +0100

    Fix deserialization error

--- include/serialization/YAMLSerializer.h
+++ include/serialization/YAMLSerializer.h
@@ -9,63 +9,67 @@
 
 namespace perfectpixel { namespace serialization {
 
-	class YAMLSerializer : public ISerializer
-	{
-	public:
-		YAMLSerializer();
+class YAMLSerializer : public ISerializer
+{
+public:
+    YAMLSerializer();
 
-		virtual void loadBuffer(const char *buffer, size_t bufferSize) override;
-		virtual void loadBuffer(const std::string &buffer) override;
+    virtual void loadBuffer(const char *buffer, size_t bufferSize) override;
+    virtual void loadBuffer(const std::string &buffer) override;
 
-		virtual void writeFloat(float val) override;
-		virtual void writeInt32(int32_t val) override;
-		virtual void writeUInt32(uint32_t val) override;
-		virtual void writeMappedUInt32(uint32_t val) override;
-		virtual void writeText(const std::string &val) override;
-		virtual void writeIdentifier(int32_t val) override;
-		virtual void writeMapKey(int32_t val) override;
-		virtual void writeIdentifier(const std::string &val) override;
-		virtual void writeMapKey(const std::string &val) override;
-		virtual void writeBinary(const void *p, uint32_t size) override;
-		virtual void writeArrayStart(bool simple) override;
-		virtual void writeArrayEnd() override;
-		virtual void writeMapStart() override;
-		virtual void writeMapEnd() override;
-		virtual void writeNull() override;
+    virtual void writeFloat(float val) override;
+    virtual void writeInt32(int32_t val) override;
+    virtual void writeUInt32(uint32_t val) override;
+    virtual void writeMappedUInt32(uint32_t val) override;
+    virtual void writeText(const std::string &val) override;
+    virtual void writeIdentifier(int32_t val) override;
+    virtual void writeMapKey(int32_t val) override;
+    virtual void writeIdentifier(const std::string &val) override;
+    virtual void writeMapKey(const std::string &val) override;
+    virtual void writeBinary(const void *p, uint32_t size) override;
+    virtual void writeArrayStart(bool simple) override;
+    virtual void writeArrayEnd() override;
+    virtual void writeMapStart() override;
+    virtual void writeMapEnd() override;
+    virtual void writeNull() override;
 
-
-		virtual void readFloat(float *val) override;
-		virtual void readInt32(int32_t *val) override;
-		virtual void readUInt32(uint32_t *val) override;
-		virtual void readMappedUInt32(uint32_t *val) override;
-		virtual void readText(std::string *val) override;
-		virtual void readIdentifier(int32_t *val) override;
+    virtual void readFloat(float *val) override;
+    virtual void readInt32(int32_t *val) override;
+    virtual void readUInt32(uint32_t *val) override;
+    virtual void readMappedUInt32(uint32_t *val) override;
+    virtual void readText(std::string *val) override;
+    virtual void readIdentifier(int32_t *val) override;
 #if PP_FULL_REFLECTION_ENABLED
-		virtual void readIdentifier(std::string *val) override;
+    virtual void readIdentifier(std::string *val) override;
 #endif // PP_FULL_REFLECTION_ENABLED
-		virtual uint32_t readBinary(void *p, uint32_t maxSize) override;
-		virtual void readMapBegin() override;
-		virtual bool readMapKey(int32_t *val) override;
-		virtual uint32_t readArrayStart() override;
-		virtual void readArrayEnd() override;
-		virtual bool isValueNull() override;
-		virtual void mapUInt32(uint32_t memory, uint32_t serialized) override;
+    virtual uint32_t readBinary(void *p, uint32_t maxSize) override;
+    virtual void readMapBegin() override;
+    virtual bool readMapKey(int32_t *val) override;
+    virtual uint32_t readArrayStart() override;
+    virtual void readArrayEnd() override;
+    virtual bool isValueNull() override;
+    virtual void mapUInt32(uint32_t memory, uint32_t serialized) override;
 
-		void push(const YAML::Node &node);
-		void pop();
-		const YAML::Node readVal();
+    void push(const YAML::Node &node);
+    void pop();
+    const YAML::Node readVal();
 
-		std::string dump();
+    std::string dump();
 
-		int32_t(*m_hash)(const std::string &);
+    int32_t (*m_hash)(const std::string &);
 #if PP_FULL_REFLECTION_ENABLED
-		std::string(*m_reverse)(int32_t);
+    std::string (*m_reverse)(int32_t);
 #endif
 
-	private:
-		YAML::Emitter *m_emitter;
-		YAML::Node m_root;
-		std::stack<std::pair<const YAML::Node, YAML::const_iterator*>> m_stack;
-	};
+private:
+    int32_t nodeAsHash(const YAML::Node &node) const;
 
-} }
+private:
+    YAML::Emitter *m_emitter;
+    YAML::Node m_root;
+    std::stack<std::pair<const YAML::Node, YAML::const_iterator *>> m_stack;
+
+    static std::string m_fallbackString;
+};
+
+}} // namespace perfectpixel::serialization
--- src/serialization/YAMLSerializer.cpp
+++ src/serialization/YAMLSerializer.cpp
@@ -1,8 +1,8 @@
 #include <serialization/YAMLSerializer.h>
 
+#include <yaml-cpp/node/convert.h>
 #include <yaml-cpp/node/parse.h>
 #include <yaml-cpp/yaml.h>
-#include <yaml-cpp/node/convert.h>
 
 #include <cstring>
 
@@ -10,301 +10,303 @@
 
 namespace perfectpixel { namespace serialization {
 
-	YAMLSerializer::YAMLSerializer()
-		: m_hash()
-		, m_reverse()
-		, m_emitter(new YAML::Emitter())
-	{
-	}
+std::string YAMLSerializer::m_fallbackString = "";
 
-	void YAMLSerializer::loadBuffer(const char *buffer, size_t bufferSize)
-	{
-		loadBuffer(std::string(buffer, bufferSize));
-	}
+YAMLSerializer::YAMLSerializer()
+    : m_hash()
+    , m_reverse()
+    , m_emitter(new YAML::Emitter())
+{}
 
-	void YAMLSerializer::loadBuffer(const std::string &buffer)
-	{
-		m_root = YAML::Load(buffer);
-		while (!m_stack.empty())
-		{
-			m_stack.pop();
-		}
-	}
+void YAMLSerializer::loadBuffer(const char *buffer, size_t bufferSize)
+{
+    loadBuffer(std::string(buffer, bufferSize));
+}
 
-	void YAMLSerializer::writeFloat(float val)
-	{
-		*m_emitter << val;
-	}
+void YAMLSerializer::loadBuffer(const std::string &buffer)
+{
+    m_root = YAML::Load(buffer);
+    while (!m_stack.empty())
+    {
+        m_stack.pop();
+    }
+}
 
-	void YAMLSerializer::writeInt32(int32_t val)
-	{
-		*m_emitter << val;
-	}
+void YAMLSerializer::writeFloat(float val)
+{
+    *m_emitter << val;
+}
 
-	void YAMLSerializer::writeUInt32(uint32_t val)
-	{
-		*m_emitter << val;
-	}
+void YAMLSerializer::writeInt32(int32_t val)
+{
+    *m_emitter << val;
+}
 
-	void YAMLSerializer::writeMappedUInt32(uint32_t val)
-	{
-		// TODO!
-		*m_emitter << val;
-	}
+void YAMLSerializer::writeUInt32(uint32_t val)
+{
+    *m_emitter << val;
+}
 
-	void YAMLSerializer::writeText(const std::string &val)
-	{
-		*m_emitter << val;
-	}
+void YAMLSerializer::writeMappedUInt32(uint32_t val)
+{
+    // TODO!
+    *m_emitter << val;
+}
 
-	void YAMLSerializer::writeIdentifier(int32_t val)
-	{
+void YAMLSerializer::writeText(const std::string &val)
+{
+    *m_emitter << val;
+}
+
+void YAMLSerializer::writeIdentifier(int32_t val)
+{
 #if PP_FULL_REFLECTION_ENABLED
-		if (m_reverse)
-		{
-			std::string text = m_reverse(val);
-			if (text != "")
-			{
-				writeIdentifier(m_reverse(val));
-				return;
-			}
-		}
+    if (m_reverse)
+    {
+        std::string text = m_reverse(val);
+        if (text != "")
+        {
+            writeIdentifier(m_reverse(val));
+            return;
+        }
+    }
 #endif
-		*m_emitter << val;
-	}
+    *m_emitter << val;
+}
 
-	void YAMLSerializer::writeIdentifier(const std::string &val)
-	{
-		*m_emitter << val;
-	}
+void YAMLSerializer::writeIdentifier(const std::string &val)
+{
+    *m_emitter << val;
+}
 
-	void YAMLSerializer::writeMapKey(int32_t val)
-	{
-		*m_emitter << YAML::Key << val << YAML::Value;
-	}
+void YAMLSerializer::writeMapKey(int32_t val)
+{
+    *m_emitter << YAML::Key << val << YAML::Value;
+}
 
-	void YAMLSerializer::writeMapKey(const std::string &val)
-	{
-		*m_emitter << YAML::Key << val << YAML::Value;
-	}
+void YAMLSerializer::writeMapKey(const std::string &val)
+{
+    *m_emitter << YAML::Key << val << YAML::Value;
+}
 
-	void YAMLSerializer::writeBinary(const void *p, uint32_t size)
-	{
-		std::string b;
-		b.assign((const char*)p, size);
-		*m_emitter << b;
-	}
+void YAMLSerializer::writeBinary(const void *p, uint32_t size)
+{
+    std::string b;
+    b.assign((const char *)p, size);
+    *m_emitter << b;
+}
 
-	void YAMLSerializer::writeArrayStart(bool simple)
-	{
-		if (simple)
-		{
-			*m_emitter << YAML::Flow;
-		}
-		*m_emitter << YAML::BeginSeq;
-	}
+void YAMLSerializer::writeArrayStart(bool simple)
+{
+    if (simple)
+    {
+        *m_emitter << YAML::Flow;
+    }
+    *m_emitter << YAML::BeginSeq;
+}
 
-	void YAMLSerializer::writeArrayEnd()
-	{
-		*m_emitter << YAML::EndSeq;
-	}
+void YAMLSerializer::writeArrayEnd()
+{
+    *m_emitter << YAML::EndSeq;
+}
 
-	void YAMLSerializer::writeMapStart()
-	{
-		*m_emitter << YAML::BeginMap;
-	}
+void YAMLSerializer::writeMapStart()
+{
+    *m_emitter << YAML::BeginMap;
+}
 
-	void YAMLSerializer::writeMapEnd()
-	{
-		*m_emitter << YAML::EndMap;
-	}
+void YAMLSerializer::writeMapEnd()
+{
+    *m_emitter << YAML::EndMap;
+}
 
-	void YAMLSerializer::writeNull()
-	{
-		*m_emitter << YAML::Null;
-	}
+void YAMLSerializer::writeNull()
+{
+    *m_emitter << YAML::Null;
+}
 
-	void YAMLSerializer::push(const YAML::Node &node)
-	{
-		m_stack.emplace(node, nullptr);
-	}
+void YAMLSerializer::push(const YAML::Node &node)
+{
+    m_stack.emplace(node, nullptr);
+}
 
-	void YAMLSerializer::pop()
-	{
-		delete m_stack.top().second;
-		m_stack.pop();
-		if (m_stack.empty())
-		{
-			m_root = YAML::Node();
-		}
-	}
+void YAMLSerializer::pop()
+{
+    delete m_stack.top().second;
+    m_stack.pop();
+    if (m_stack.empty())
+    {
+        m_root = YAML::Node();
+    }
+}
 
-	const YAML::Node YAMLSerializer::readVal()
-	{
-		if (m_stack.empty())
-		{
-			return m_root;
-		}
-		else if (m_stack.top().first.IsMap())
-		{
-			return (*m_stack.top().second)->second;
-		}
-		else if (m_stack.top().first.IsSequence())
-		{
-			YAML::const_iterator &it = *m_stack.top().second;
-			return *(it++);
-		}
-		else
-		{
-			return m_stack.top().first;
-		}
-	}
+const YAML::Node YAMLSerializer::readVal()
+{
+    if (m_stack.empty())
+    {
+        return m_root;
+    }
+    else if (m_stack.top().first.IsMap())
+    {
+        return (*m_stack.top().second)->second;
+    }
+    else if (m_stack.top().first.IsSequence())
+    {
+        YAML::const_iterator &it = *m_stack.top().second;
+        return *(it++);
+    }
+    else
+    {
+        return m_stack.top().first;
+    }
+}
 
-	void YAMLSerializer::readFloat(float *val)
-	{
-		*val = readVal().as<float>();
-	}
+void YAMLSerializer::readFloat(float *val)
+{
+    *val = readVal().as<float>();
+}
 
-	void YAMLSerializer::readInt32(int32_t *val)
-	{
-		*val = readVal().as<int32_t>();
-	}
+void YAMLSerializer::readInt32(int32_t *val)
+{
+    *val = readVal().as<int32_t>();
+}
 
-	void YAMLSerializer::readUInt32(uint32_t *val)
-	{
-		*val = readVal().as<uint32_t>();
-	}
+void YAMLSerializer::readUInt32(uint32_t *val)
+{
+    *val = readVal().as<uint32_t>();
+}
 
-	void YAMLSerializer::readMappedUInt32(uint32_t *val)
-	{
-		(void)val;
-		throw "Todo";
-	}
+void YAMLSerializer::readMappedUInt32(uint32_t *val)
+{
+    (void)val;
+    throw "Todo";
+}
 
-	void YAMLSerializer::readText(std::string *val)
-	{
-		*val = readVal().as<std::string>();
-	}
+void YAMLSerializer::readText(std::string *val)
+{
+    *val = readVal().as<std::string>();
+}
 
-	void YAMLSerializer::readIdentifier(int32_t *val)
-	{
-		const YAML::Node node = readVal();
+void YAMLSerializer::readIdentifier(int32_t *val)
+{
+    const YAML::Node node = readVal();
 
-		try
-		{
-			*val = node.as<int32_t>();
-		}
-		catch (YAML::BadConversion &)
-		{
-			*val = m_hash(node.as<std::string>());
-		}
-	}
+    *val = nodeAsHash(node);
+}
 
-	void YAMLSerializer::readIdentifier(std::string *val)
-	{
-		readText(val);
-	}
+void YAMLSerializer::readIdentifier(std::string *val)
+{
+    readText(val);
+}
 
-	uint32_t YAMLSerializer::readBinary(void *p, uint32_t maxSize)
-	{
-		std::string text = readVal().as<std::string>();
-		memcpy(p, text.c_str(), std::min(static_cast<std::size_t>(maxSize), text.size()));
-		return std::min(static_cast<std::size_t>(maxSize), text.size());
-	}
+uint32_t YAMLSerializer::readBinary(void *p, uint32_t maxSize)
+{
+    std::string text = readVal().as<std::string>();
+    memcpy(
+        p,
+        text.c_str(),
+        std::min(static_cast<std::size_t>(maxSize), text.size()));
+    return std::min(static_cast<std::size_t>(maxSize), text.size());
+}
 
-	void YAMLSerializer::readMapBegin()
-	{
-		const YAML::Node node = readVal();
+void YAMLSerializer::readMapBegin()
+{
+    const YAML::Node node = readVal();
 
-		if (!node.IsMap())
-		{
-			throw "YAML Decoding error";
-		}
+    if (!node.IsMap())
+    {
+        throw "YAML Decoding error";
+    }
 
-		push(node); 
-	}
+    push(node);
+}
 
-	bool YAMLSerializer::readMapKey(int32_t *val)
-	{
-		if (m_stack.empty())
-		{
-			return false;
-		}
+bool YAMLSerializer::readMapKey(int32_t *val)
+{
+    if (m_stack.empty())
+    {
+        return false;
+    }
 
-		if (!m_stack.top().first.IsMap())
-		{
-			throw "YAML Decoding error";
-		}
+    if (!m_stack.top().first.IsMap())
+    {
+        throw "YAML Decoding error";
+    }
 
-		if (m_stack.top().second)
-		{
-			++(*m_stack.top().second);
-		}
-		else
-		{
-			m_stack.top().second = new YAML::const_iterator(m_stack.top().first.begin());
-		}
+    if (m_stack.top().second)
+    {
+        ++(*m_stack.top().second);
+    }
+    else
+    {
+        m_stack.top().second
+            = new YAML::const_iterator(m_stack.top().first.begin());
+    }
 
-		YAML::const_iterator &it = *m_stack.top().second;
-		if (it == m_stack.top().first.end())
-		{
-			pop();
-			return false;
-		}
+    YAML::const_iterator &it = *m_stack.top().second;
+    if (it == m_stack.top().first.end())
+    {
+        pop();
+        return false;
+    }
 
-		try
-		{
-			*val = it->first.as<int32_t>();
-		}
-		catch (YAML::BadConversion &)
-		{
-			*val = m_hash(it->first.as<std::string>());
-		}
-		return true;
-	}
+    *val = nodeAsHash(it->first);
 
-	uint32_t YAMLSerializer::readArrayStart()
-	{
-		const YAML::Node node = readVal();
-		if (!node.IsSequence())
-		{
-			throw "YAML decode error";
-		}
+    return true;
+}
 
-		push(node);
-		m_stack.top().second = new YAML::const_iterator(node.begin());
-		return node.size();
-	}
+uint32_t YAMLSerializer::readArrayStart()
+{
+    const YAML::Node node = readVal();
+    if (!node.IsSequence())
+    {
+        throw "YAML decode error";
+    }
 
-	void YAMLSerializer::readArrayEnd()
-	{
-		if (!m_stack.top().first.IsSequence())
-		{
-			throw "YAML decode error";
-		}
+    push(node);
+    m_stack.top().second = new YAML::const_iterator(node.begin());
+    return node.size();
+}
 
-		pop();
-	}
+void YAMLSerializer::readArrayEnd()
+{
+    if (!m_stack.top().first.IsSequence())
+    {
+        throw "YAML decode error";
+    }
 
-	bool YAMLSerializer::isValueNull()
-	{
-		return readVal().IsNull();
-	}
+    pop();
+}
 
-	void YAMLSerializer::mapUInt32(uint32_t memory, uint32_t serialized)
-	{
-		(void)memory;
-		(void)serialized;
-		throw "Todo";
-	}
+bool YAMLSerializer::isValueNull()
+{
+    return readVal().IsNull();
+}
 
-	std::string YAMLSerializer::dump()
-	{
-		std::string result;
-		result.assign(m_emitter->c_str(), m_emitter->size());
-		return result;
-	}
+void YAMLSerializer::mapUInt32(uint32_t memory, uint32_t serialized)
+{
+    (void)memory;
+    (void)serialized;
+    throw "Todo";
+}
 
-}}
+std::string YAMLSerializer::dump()
+{
+    std::string result;
+    result.assign(m_emitter->c_str(), m_emitter->size());
+    return result;
+}
+
+int32_t YAMLSerializer::nodeAsHash(const YAML::Node &node) const
+{
+    std::string str = node.as<std::string>(m_fallbackString);
+    if (str == m_fallbackString)
+    {
+        return node.as<int32_t>();
+    }
+    return m_hash(str);
+}
+
+}} // namespace perfectpixel::serialization
 
 #endif // PP_FULL_REFLECTION_ENABLED
