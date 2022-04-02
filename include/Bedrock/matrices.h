07b0cc6d89b36edc45cdc0899013a3af736b2413
Jeppe Tarp <jeppebtarp@gmail.com>
2022-04-02 15:46:43 +0200

    Add some additional matrix math

--- include/Bedrock/matrices.h
+++ include/Bedrock/matrices.h
@@ -1,194 +1,207 @@
 #pragma once
 
+#include <Bedrock/Quaternion.h>
 #include <Bedrock/vectors.h>
 
-#include <type_traits>
 #include <array>
+#include <type_traits>
 
-namespace perfectpixel {
-	namespace bedrock {
+namespace perfectpixel { namespace bedrock {
 
-		
-		template<unsigned W, unsigned H>
-		struct Matrix
-		{
-			static_assert(W > 1, "Matrix should have at least 2 columns, use a vector otherwise");
-			static_assert(H > 1, "Matrix should have at least 2 rows, use a vector otherwise");
+template <unsigned W, unsigned H>
+struct Matrix
+{
+    static_assert(
+        W > 1, "Matrix should have at least 2 columns, use a vector otherwise");
+    static_assert(
+        H > 1, "Matrix should have at least 2 rows, use a vector otherwise");
 
-			template <typename T>
-			using EnableIfSquare = typename std::enable_if<W == H, T>::type;
+    template <typename T>
+    using EnableIfSquare = typename std::enable_if<W == H, T>::type;
 
-			Matrix(std::array<float, W*H> data)
-				: m_data(data)
-			{}
+    Matrix(std::array<float, W * H> data)
+        : m_data(data)
+    {}
 
-			Matrix()
-				: m_data()
-			{
-				for (unsigned i = 0; i < W*H; i++)
-				{
-					m_data[i] = 0.0f;
-				}
-			}
+    Matrix()
+        : m_data()
+    {
+        for (unsigned i = 0; i < W * H; i++)
+        {
+            m_data[i] = 0.0f;
+        }
+    }
 
-			float &m(unsigned x, unsigned y)
-			{
-				return m_data[x * H + y];
-			}
+    float &m(unsigned x, unsigned y)
+    {
+        return m_data[x * H + y];
+    }
 
-			float m(unsigned x, unsigned y) const
-			{
-				return m_data[x * H + y];
-			}
+    float m(unsigned x, unsigned y) const
+    {
+        return m_data[x * H + y];
+    }
 
-			std::array<float, W*H> m_data;
+    std::array<float, W * H> m_data;
 
-			Vector<H> column(unsigned index) const
-			{
-				std::array<float, H> result;
-				for (unsigned i = 0; i < W; i++)
-				{
-					result[i] = m_data[index * H + i];
-				}
-				return result;
-			}
+    Vector<H> column(unsigned index) const
+    {
+        std::array<float, H> result;
+        for (unsigned i = 0; i < W; i++)
+        {
+            result[i] = m_data[index * H + i];
+        }
+        return result;
+    }
 
-			Vector<W> row(unsigned index) const
-			{
-				std::array<float, W> result;
-				for (unsigned i = 0; i < W; i++)
-				{
-					result[i] = m_data[i * H + index];
-				}
-				return result;
-			}
+    Vector<W> row(unsigned index) const
+    {
+        std::array<float, W> result;
+        for (unsigned i = 0; i < W; i++)
+        {
+            result[i] = m_data[i * H + index];
+        }
+        return result;
+    }
 
-			Vector<H> operator*(const Vector<W> &r) const
-			{
-				Vector<H> result;
-				for (unsigned i = 0; i < H; i++)
-				{
-					result.m_data[i] = Vector<W>::dot(row(i), r);
-				}
-				return result;
-			}
+    Vector<H> operator*(const Vector<W> &r) const
+    {
+        Vector<H> result;
+        for (unsigned i = 0; i < H; i++)
+        {
+            result.m_data[i] = Vector<W>::dot(row(i), r);
+        }
+        return result;
+    }
 
-			Matrix<H, H> operator*(const Matrix<H, W> &r) const
-			{
-				Matrix<H, H> result;
+    Matrix<H, H> operator*(const Matrix<H, W> &r) const
+    {
+        Matrix<H, H> result;
 
-				for (unsigned i = 0; i < H; i++)
-				{
-					for (unsigned j = 0; j < H; j++)
-					{
-						result.m(i, j) = Vector<H>::dot(column(i), r.row(j));
-					}
-				}
+        for (unsigned i = 0; i < H; i++)
+        {
+            for (unsigned j = 0; j < H; j++)
+            {
+                result.m(i, j) = Vector<H>::dot(column(i), r.row(j));
+            }
+        }
 
-				return result;
-			}
+        return result;
+    }
 
-			template<typename T = Matrix<H, W>>
-			EnableIfSquare<T> &operator*=(const Matrix<H, W> &r)
-			{
-				return *this = *this * r;
-			}
+    template <typename T = Matrix<H, W>>
+    EnableIfSquare<T> &operator*=(const Matrix<H, W> &r)
+    {
+        return *this = *this * r;
+    }
 
-			bool operator==(const Matrix<W, H> &r) const 
-			{
-				return m_data == r.m_data;
-			}
+    bool operator==(const Matrix<W, H> &r) const
+    {
+        return m_data == r.m_data;
+    }
 
-			template <typename T = Matrix<H, H>>
-			static EnableIfSquare<T> generateIdentity()
-			{
-				T result;
+    template <typename T = Matrix<H, H>>
+    static EnableIfSquare<T> generateIdentity()
+    {
+        T result;
 
-				for (unsigned i = 0; i < H; i++)
-				{
-					result.m(i, i) = 1;
-				}
+        for (unsigned i = 0; i < H; i++)
+        {
+            result.m(i, i) = 1;
+        }
 
-				return result;
-			}
+        return result;
+    }
 
-			template <typename T=Matrix<H-1, H-1>>
-			EnableIfSquare<T> getMinor(unsigned x, unsigned y) const
-			{
-				T result;
+    template <typename T = Matrix<H - 1, H - 1>>
+    EnableIfSquare<T> getMinor(unsigned x, unsigned y) const
+    {
+        T result;
 
-				for (unsigned i = 0; i < H - 1; i++)
-				{
-					for (unsigned j = 0; j < H - 1; j++)
-					{
-						unsigned sourceX = i >= x ? i + 1 : i;
-						unsigned sourceY = j >= y ? j + 1 : j;
+        for (unsigned i = 0; i < H - 1; i++)
+        {
+            for (unsigned j = 0; j < H - 1; j++)
+            {
+                unsigned sourceX = i >= x ? i + 1 : i;
+                unsigned sourceY = j >= y ? j + 1 : j;
 
-						result.m(i, j) = m(sourceX, sourceY);
-					}
-				}
+                result.m(i, j) = m(sourceX, sourceY);
+            }
+        }
 
-				return result;
-			}
+        return result;
+    }
 
-			template<unsigned D>
-			static float getDeterminant(const Matrix<D, D> &mat)
-			{
-				float accumulator{ 0.0f };
-				bool add = true;
+    template <unsigned D>
+    static float getDeterminant(const Matrix<D, D> &mat)
+    {
+        float accumulator{0.0f};
+        bool add = true;
 
-				for (unsigned i = 0; i < D; i++)
-				{
-					float recurse = mat.m(i, 0) * getDeterminant(mat.getMinor(i, 0));
+        for (unsigned i = 0; i < D; i++)
+        {
+            float recurse = mat.m(i, 0) * getDeterminant(mat.getMinor(i, 0));
 
-					accumulator += add ? recurse : -recurse;
+            accumulator += add ? recurse : -recurse;
 
-					add = !add;
-				}
+            add = !add;
+        }
 
-				return accumulator;
-			}
+        return accumulator;
+    }
 
-			static float getDeterminant(const Matrix<2, 2> &mat)
-			{
-				return mat.m_data[0] * mat.m_data[3] - mat.m_data[1] * mat.m_data[2];
-			}
+    static float getDeterminant(const Matrix<2, 2> &mat)
+    {
+        return mat.m_data[0] * mat.m_data[3] - mat.m_data[1] * mat.m_data[2];
+    }
 
-			template<typename T = float>
-			EnableIfSquare<T> determinant()
-			{
-				return Matrix<H, H>::getDeterminant(*this);
-			}
+    template <typename T = float>
+    EnableIfSquare<T> determinant()
+    {
+        return Matrix<H, H>::getDeterminant(*this);
+    }
+};
 
+struct Matrix2x2 : public Matrix<2, 2>
+{
+    /*implicit*/ Matrix2x2(const Matrix<2, 2> &convert)
+        : Matrix<2, 2>(convert)
+    {}
+    explicit Matrix2x2(const std::array<float, 4> data)
+        : Matrix<2, 2>(data)
+    {}
 
-		};
+    static const Matrix2x2 IDENTITY;
 
-		struct Matrix2x2 : public Matrix<2, 2>
-		{
-			/*implicit*/ Matrix2x2(const Matrix<2, 2> &convert) : Matrix<2, 2>(convert) {}
-			explicit Matrix2x2(const std::array<float, 4> data) : Matrix<2, 2>(data) {}
+    static Matrix2x2 rotate(Angle angle);
+};
 
-			static const Matrix2x2 IDENTITY;
+struct Matrix3x3 : public Matrix<3, 3>
+{
+    /*implicit*/ Matrix3x3(const Matrix<3, 3> &convert)
+        : Matrix<3, 3>(convert)
+    {}
+    explicit Matrix3x3(const std::array<float, 9> data)
+        : Matrix<3, 3>(data)
+    {}
 
-			static Matrix2x2 rotate(Angle angle);
-		};
+    static const Matrix3x3 IDENTITY;
+};
 
-		struct Matrix3x3 : public Matrix<3, 3>
-		{
-			/*implicit*/ Matrix3x3(const Matrix<3, 3> &convert) : Matrix<3, 3>(convert) {}
-			explicit Matrix3x3(const std::array<float, 9> data) : Matrix<3, 3>(data) {}
+struct Matrix4x4 : public Matrix<4, 4>
+{
+    /*implicit*/ Matrix4x4(const Matrix<4, 4> &convert)
+        : Matrix<4, 4>(convert)
+    {}
+    explicit Matrix4x4(const std::array<float, 16> data)
+        : Matrix<4, 4>(data)
+    {}
 
-			static const Matrix3x3 IDENTITY;
-		};
+    static const Matrix4x4 IDENTITY;
 
-		struct Matrix4x4 : public Matrix<4, 4>
-		{
-			/*implicit*/ Matrix4x4(const Matrix<4, 4> &convert) : Matrix<4, 4>(convert) {}
-			explicit Matrix4x4(const std::array<float, 16> data) : Matrix<4, 4>(data) {}
+    static Matrix4x4 translate(const Vector3 &coordinates);
+    static Matrix4x4 scale(const Vector3 &axes);
+    static Matrix4x4 roatation(const Quaternion &quat);
+};
 
-			static const Matrix4x4 IDENTITY;
-		};
-
-
-	}
-}
+}} // namespace perfectpixel::bedrock
--- src/Bedrock/matrices.cpp
+++ src/Bedrock/matrices.cpp
@@ -2,24 +2,76 @@
 
 #include <cmath>
 
-namespace perfectpixel {
-namespace bedrock {
+namespace perfectpixel { namespace bedrock {
 
-	const Matrix2x2 Matrix2x2::IDENTITY = Matrix2x2::generateIdentity();
-	const Matrix3x3 Matrix3x3::IDENTITY = Matrix3x3::generateIdentity();
-	const Matrix4x4 Matrix4x4::IDENTITY = Matrix4x4::generateIdentity();
+const Matrix2x2 Matrix2x2::IDENTITY = Matrix2x2::generateIdentity();
+const Matrix3x3 Matrix3x3::IDENTITY = Matrix3x3::generateIdentity();
+const Matrix4x4 Matrix4x4::IDENTITY = Matrix4x4::generateIdentity();
 
-
-	perfectpixel::bedrock::Matrix2x2 Matrix2x2::rotate(Angle angle)
-	{
-		float cosine = std::cos(angle.radians());
-		float sine = std::sin(angle.radians());
-		return Matrix<2, 2>(std::array<float, 4>{
-			cosine,		sine, // C1
-			-sine,		cosine, // C2
-		});
-	}
-
-
+perfectpixel::bedrock::Matrix2x2 Matrix2x2::rotate(Angle angle)
+{
+    float cosine = std::cos(angle.radians());
+    float sine   = std::sin(angle.radians());
+    return Matrix<2, 2>(std::array<float, 4>{
+        cosine,
+        sine, // C1
+        -sine,
+        cosine, // C2
+    });
 }
+
+perfectpixel::bedrock::Matrix4x4
+Matrix4x4::translate(const Vector3 &coordinates)
+{
+    Matrix4x4 result = IDENTITY;
+
+    result.m(3, 0) = coordinates.x();
+    result.m(3, 1) = coordinates.y();
+    result.m(3, 2) = coordinates.z();
+
+    return result;
 }
+
+perfectpixel::bedrock::Matrix4x4 Matrix4x4::scale(const Vector3 &axes)
+{
+    Matrix4x4 result = IDENTITY;
+
+    result.m(0, 0) = axes.x();
+    result.m(1, 1) = axes.y();
+    result.m(2, 2) = axes.z();
+
+    return result;
+}
+
+perfectpixel::bedrock::Matrix4x4 Matrix4x4::roatation(const Quaternion &quat)
+{
+    Matrix4x4 result = IDENTITY;
+
+    const float xx = quat.x() * quat.x();
+    const float xw = quat.x() * quat.w();
+    const float xy = quat.x() * quat.y();
+
+    const float yy = quat.y() * quat.y();
+    const float yw = quat.y() * quat.w();
+    const float yz = quat.y() * quat.z();
+
+    const float zz = quat.z() * quat.z();
+    const float zw = quat.z() * quat.w();
+    const float zx = quat.x() * quat.z();
+
+    result.m(0, 0) = 1.0f - 2.0f * (yy + zz);
+    result.m(0, 1) = 2.0f * (xy - zw);
+    result.m(0, 2) = 2.0f * (zx + yw);
+
+    result.m(1, 0) = 2.0f * (xy + zw);
+    result.m(1, 1) = 1.0f - 2.0f * (zz + xx);
+    result.m(1, 2) = 2.0f * (yz - xw);
+
+    result.m(2, 0) = 2.0f * (zx - yw);
+    result.m(2, 1) = 2.0f * (yz + xw);
+    result.m(2, 2) = 1.0f - 2.0f * (xx + yy);
+
+    return result;
+}
+
+}} // namespace perfectpixel::bedrock
